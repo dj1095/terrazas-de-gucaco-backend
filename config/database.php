cbafccf2f561ef25d5fd7d9e372cf234d5f52ad9
pramodh <durgapramodh10dj95@gmail.com>
2023-03-23 16:51:55 -0500

    initial-template

---
+++ config/database.php
@@ -0,0 +1,46 @@
+<?php
+    class Database{
+        //DB params 
+        private $host = null;
+        private $db_name = null;
+        private $user_name = null;
+        private $password = null;
+        private $port = null;
+        private static $conn = null;
+
+        private function load_db_config()
+        { 
+            $this->host = getenv('DB_HOST');
+            $this->db_name = getenv('DB_NAME');
+            $this->user_name = getenv('DB_USERNAME');
+            $this->password = getenv('DB_PASSWORD');
+            $this->port = getenv('DB_PORT');
+
+        }
+
+        private function __construct()
+        {
+            $this->load_db_config();
+        }
+
+        public static function getDBConnection()
+        {
+            if (self::$conn == null)
+            {
+                $db = new Database();
+                self::$conn = $db->connect();
+            }
+            return self::$conn;
+        }
+
+        public function connect(){
+            try{
+                $conn = new PDO('mysql:host='. $this->host.';port='.$this->port.';dbname='. $this->db_name, $this->user_name, $this->password);
+                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            }catch(PDOException $e){
+                echo'Connection Error: '. $e->getMessage();
+            }
+            return $conn;
+        }
+
+    }
